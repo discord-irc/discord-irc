3c055d83cfdb249c99957f87e0cbc10741ec4f40
ChillerDragon <ChillerDragon@gmail.com>
2023-05-19 20:22:41 +0200

    Sending to irc-http backend works
    receiving does not
    
    https://github.com/ChillerDragon/irc-http
    
    maybe use node-irc lib to build your own http wrapper
    that can also do GET /messages/channel
    because irc-http does not seem to offer that

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,23 @@
-import './style.css';
+import './style.css'
 
 const foo: string = "hello world from ts"
 console.log(foo)
+
+const backendUrl = 'http://localhost:6969'
+
+document.querySelector('form')
+    .addEventListener('submit', (event) => {
+        event.preventDefault()
+        const messageInp: HTMLInputElement = document.querySelector('#message')
+        const message = messageInp.value
+        fetch(`${backendUrl}/ddnet`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                msg: message
+            })
+        })
+            .then(res => console.log(res))
+    })
