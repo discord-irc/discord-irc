c1f479b18d4d006eedcb4e46813ff5349d24b9c1
ChillerDragon <chillerdragon@gmail.com>
2023-06-07 08:36:12 +0200

    New emotes thanks to bencie

--- src/emotes.ts
+++ src/emotes.ts
@@ -5,14 +5,40 @@ const emoteMappings: Record<string, string> = {
     'gigachad': '960830678465933403',
     'justatest': '572499997178986510',
     'feelsbadman': '391614770303991808',
+    'feelsamazingman': '391614823655538699',
+    'greenthing': '623706333677617203',
     'pepeH': '46273591700684802',
     'poggers2': '46273591700684802',
     'hisnail': '768893210726367232',
     'f3': '397431188941438976',
     'f4': '397431204552376320',
+    'angry': '395753905650270232',
+    'banhammer': '392813948858269696',
+    'bluekitty': '34668349791980748',
+    'bluestripe': '346683497802366976',
+    'boo': '395753917062971402',
+    'brownbear': '346683497701834762',
+    'cammo': '346683496842133504',
+    'cammostripes': '346683496476966913',
+    'ddnet': '391727274824826880',
+    'ddnet_lgbt': '854799059402227782',
+    'flag_unk': '519687837868883993',
+    'frozen': '385929852022161418',
+    'fury': '395753924432494596',
+    'gg': '422062383687794689',
+    'giftee_green': '783751485736747008',
+    'giftee_red': '783751448664080466',
+    'happy': '395753933089406976',
+    'heartw': '395753947396046850',
+    'hey': '395753956711858196',
+    'kek': '623709640789852179',
+    'jaouis': '771825988556292096',
+    'lol': '460429405328506900',
+    'mmm': '395753965410582538',
+
     'rocket': 'rocket',
 }
-// TODO: :fuckyousnail: :banhammer: :feelsamazingman: :greenthing: :kek: :monkalaugh: :monkaS: :troll: :nouis: 
+// TODO: :fuckyousnail: :monkalaugh: :monkaS: :troll: :nouis:
 const animatedEmoteMappings: Record<string, string> = {
     'Catxplosion': '1082715870893195274', // https://cdn.discordapp.com/emojis/1082715870893195274.gif?size=80&quality=lossless
 }
