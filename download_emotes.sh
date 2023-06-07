ab1528c20d1da6138ce43bc1a0c5fe81ac22a6ea
ChillerDragon <chillerdragon@gmail.com>
2023-06-07 09:30:24 +0200

    Fixes and better error handling for emote downloader

--- download_emotes.sh
+++ download_emotes.sh
@@ -3,6 +3,8 @@
 is_custom_emotes=0
 is_animated_emotes=0
 
+mkdir -p tmp
+
 if [ ! -d src/img/emotes/ ]
 then
 	echo "Error: folder not found src/img/emotes/"
@@ -31,7 +33,7 @@ do
 	if [[ "$line" =~ ^\ *"'"(.*)"'":\ "'"([0-9]+)"'", ]]
 	then
 		emote_name="${BASH_REMATCH[1]}"
-		emote_id="${BASH_REMATCH[1]}"
+		emote_id="${BASH_REMATCH[2]}"
 		match=1
 	fi
 
@@ -39,8 +41,29 @@ do
 
 	if [[ "$is_custom_emotes" == "1" ]]
 	then
-		echo "emote: $emote_name"
-		wget -O "src/img/emotes/$emote_name.png" "https://cdn.discordapp.com/emojis/$emote_id.png"
+		echo -n "emote: $emote_name ... "
+		dst_file="src/img/emotes/$emote_name.png"
+		if [ -f "$dst_file" ]
+		then
+			if file "$dst_file" | grep -q "PNG image"
+			then
+				echo "OK (cache)"
+				continue
+			else
+				echo "cache is not PNG downloading"
+			fi
+		else
+			echo "downloading"
+		fi
+		cdn_url="https://cdn.discordapp.com/emojis/$emote_id.png"
+		if ! wget -O "tmp/$emote_name.png" "$cdn_url"
+		then
+			echo "Error: failed to download $emote_name with id $emote_id"
+			echo "       $cdn_url"
+			rm "tmp/$emote_name.png"
+			exit 1
+		fi
+		mv "tmp/$emote_name.png" src/img/emotes/
 	fi
 	# works but we do not need it for now
 	# we put the discord link there anyways
