b08be196609534f707f7317cab5f02216ce5ec99
ChillerDragon <ChillerDragon@gmail.com>
2024-02-01 15:51:02 +0800

    Crash logger track promises and publish via alert plugin

--- src/plugins/core/crash_logger.ts
+++ src/plugins/core/crash_logger.ts
@@ -7,20 +7,24 @@ class CrashLog {
   source?: string
   lineno?: number
   colno?: number
-  error?: Error
+  error?: Error | string // hack by chiler to add `| string`
 
   // https://developer.mozilla.org/en-US/docs/Web/API/Window/error_event
   // onerror = (event, source, lineno, colno, error) => {};
   // i used the same types as my lib.dom.d.ts
-  constructor (event: Event | string, source?: string, lineno?: number, colno?: number, error?: Error) {
+  //
+  // i added `| string` to error to be able to use it from window.onunhandledrejection too -----------.
+  //                                                                                                   v
+  constructor (event: Event | string, source?: string, lineno?: number, colno?: number, error?: Error | string) {
     this.event = event
     this.source = source // file
     this.lineno = lineno
     this.colno = colno
+    this.error = error
   }
 
   toString () {
-    return `[crash] ${this.event} ${this.source}:${this.lineno}:${this.colno}`
+    return `[crash_logger] ${this.event} ${this.source}:${this.lineno}:${this.colno} ${this.error}`
   }
 }
 
@@ -35,24 +39,48 @@ class CrashLoggerPlugin extends BasePlugin {
 
   onInit (): void {
     console.log('crash logger init ...')
+    this.hookWindowError()
+    this.hookPromiseError()
+  }
+
+  logCrash (crashLog: CrashLog): void {
+    this.crashLogs.push(crashLog)
+    const plugin = getPluginThatImplementsAlert()
+    if (plugin) {
+      const alertMsg: AlertMessage = {
+        success: false,
+        message: crashLog.toString(),
+        expire: 8000
+      }
+      plugin.addAlert(alertMsg)
+    } else {
+      console.warn('No alert plugin found')
+    }
+  }
+
+  hookPromiseError () {
+    if (window.onunhandledrejection !== null) {
+      throw new Error(`[crash_logger] failed to set window.onunhandledrejection. Not overwriting the following callback: ${window.onunhandledrejection}`)
+    }
+    window.onunhandledrejection = (event: PromiseRejectionEvent) => {
+      // promise reaon
+      console.log(`[crash_logger] ${event}`)
+      console.log(event.reason)
+      console.log(typeof event.reason)
+      const crashLog = new CrashLog(event, JSON.stringify(event.promise), null, null, event.reason)
+      this.logCrash(crashLog)
+    }
+  }
+
+  hookWindowError () {
     if (window.onerror !== null) {
       throw new Error(`[crash_logger] failed to set window.onerror. Not overwriting the following callback: ${window.onerror}`)
     }
     window.onerror = (message, file, line, column, error) => {
       console.log(`[crash_logger] ${error}`)
+      // TODO: support source maps from webpack omg
       const crashLog = new CrashLog(message, file, line, column, error)
-      this.crashLogs.push(crashLog)
-      const plugin = getPluginThatImplementsAlert()
-      if (plugin) {
-        const alertMsg: AlertMessage = {
-          success: false,
-          message: crashLog.toString(),
-          expire: 8000
-        }
-        plugin.addAlert(alertMsg)
-      } else {
-        console.warn('No alert plugin found')
-      }
+      this.logCrash(crashLog)
     }
   }
 }
