a6514898f68940313ab1e6d5d6b755c510b00853
ChillerDragon <chillerdragon@gmail.com>
2025-11-30 10:08:14 +0100

    something something custom emotes

--- src/plugins/core/custom_emotes.ts
+++ src/plugins/core/custom_emotes.ts
@@ -1,21 +1,39 @@
-import { autoComplete } from '../../autocomplete'
 import { CustomEmotesPluginImplementation } from '../plugin_implementations'
 
+interface CustomEmote {
+  name: string
+  url: string
+}
+
 class CustomEmotesPlugin extends CustomEmotesPluginImplementation {
+  customEmotes: Array<CustomEmote>
+
   constructor () {
     super('custom_emotes')
-  }
 
-  getAllCustomEmotes () {
-    return [
-      'pingsock.png',
-      'ping-sock-angry.gif'
+    this.customEmotes = [
+      {
+        name: 'pingsock.png',
+        url: 'https://raw.githubusercontent.com/discord-irc/discord-irc/refs/heads/master/src/img/custom_emotes/pingsock.png'
+      },
+      {
+        name: 'ping-sock-angry.gif',
+        url: 'https://raw.githubusercontent.com/discord-irc/discord-irc/refs/heads/master/src/img/custom_emotes/ping-sock-angry.gif'
+      },
     ]
   }
 
-  onKeydownMessageInput (event: KeyboardEvent, messageInp: HTMLInputElement): void {
-    autoComplete(':', this.getAllCustomEmotes(), event, messageInp, ': ')
+  getAllCustomEmoteNames () {
+    return Object.keys(this.customEmotes)
   }
+
+  // yea dont do auto complete for these??
+  // just add them to the picker
+  // but i think both the picker and the autocomplete do not allow extensions :/
+
+  // onKeydownMessageInput (event: KeyboardEvent, messageInp: HTMLInputElement): void {
+  //   autoComplete(':', this.getAllCustomEmoteNames(), event, messageInp, ': ')
+  // }
 }
 
 export default CustomEmotesPlugin
